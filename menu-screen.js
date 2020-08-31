234e5ea3d611cafe3d7bb4e6ca80c6808846517a
Y. Violet Guo <violetguos@users.noreply.github.com>
2020-08-31 11:54:57 -0300

    Add song and theme submit event

--- menu-screen.js
+++ menu-screen.js
@@ -7,12 +7,19 @@ class MenuScreen {
     // TODO(you): Implement the constructor and add fields as necessary.
     this.onResponse = this.onResponse.bind(this);
     this.onStreamProcessed = this.onStreamProcessed.bind(this);
+    this._onGo = this._onGo.bind(this);
+
     this.container = container;
     this.themeGenerator();
 
     // populate the menu list
     const promise = fetch('https://yayinternet.github.io/hw4-music/songs.json', {mode: 'cors'});
     promise.then(this.onResponse, this.onError);
+
+
+    // get the go button
+    const goButton = this.container.querySelector("input[value=Go]");
+    goButton.addEventListener('click', this._onGo);
   }
 
   // TODO(you): Add methods as necessary.
@@ -41,7 +48,7 @@ class MenuScreen {
   themeGenerator(){
     const themes = ['candy', 'charlie brown', 'computers', 'dance', 
       'donuts', 'hello kitty', 'flowers', 'nature', 'turtles', 'space'];
-    const index = Math.round(Math.random() * themes.length);  
+    const index = Math.floor(Math.random() * themes.length);  
     const randomTheme =  themes[index];
 
     const queryTheme = this.container.querySelector("#query-input");
@@ -49,5 +56,15 @@ class MenuScreen {
 
   }
   
+  _onGo(event){
+    //Hide the Menu screen
+    //Print out the submitted song value and theme value
+    event.preventDefault();
+    const selectElement = this.container.querySelector("#song-selector");
+    const queryTheme = this.container.querySelector("#query-input");
+    console.log(selectElement.options[selectElement.selectedIndex].text);
+    console.log(queryTheme.value);
+    
+  }
   
 }
